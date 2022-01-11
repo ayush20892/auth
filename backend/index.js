5b29ec79f82fd263a964a958064a20d506033ea0
Ayush Sharma <ayush20892@gmail.com>
2022-01-12 00:19:51 +0530

    Delete backend directory

--- backend/index.js
+++
@@ -1,9 +0,0 @@
-const app = require("./app");
-require("dotenv").config();
-const dbConnection = require("./config/db");
-
-dbConnection();
-
-app.listen(process.env.PORT, () => {
-  console.log(`Server is running at port ${process.env.PORT}...`);
-});
