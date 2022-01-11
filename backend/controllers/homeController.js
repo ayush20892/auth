083f5820b2df60e238923561ef0a7df2abd339df
Ayush Sharma <ayush20892@gmail.com>
2022-01-12 00:25:12 +0530

    Delete backend directory

--- backend/controllers/homeController.js
+++
@@ -1,8 +0,0 @@
-const BigPromise = require("../middlewares/bigPromise");
-
-exports.home = BigPromise((req, res) => {
-  res.status(200).json({
-    success: "true",
-    greeting: "Hello Backend Developer",
-  });
-});
